1af1c73fe722775fbfa3a4517ea4216add1ca974
Justine <jawunudo@gmail.com>
2022-10-17 15:52:35 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  * main - entry point
  *
- * return: always 0
+ * return - always 0
  */
-int main(void) {
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
