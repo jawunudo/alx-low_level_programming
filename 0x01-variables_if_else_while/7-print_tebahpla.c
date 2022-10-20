ceba52677643d4b35ef05fcee2fe23ce735b67c9
Justine <jawunudo@gmail.com>
2022-10-20 06:51:15 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers from 0 - 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a >= 'z'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
