f97b1c07bf4d670a5356b18e8e5aee9c7a1498aa
Justine <jawunudo@gmail.com>
2022-10-20 12:19:42 -0700

    Combo5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+
+		for (j = 0; j < 100; j++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(32);
+			putchar(c + '0');
+			putchar(d + '0');
+
+			if (!(a == 9 && b == 8))
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
