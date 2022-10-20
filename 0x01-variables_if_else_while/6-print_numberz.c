0eba2c5342e7d0807118a0f9b4cbfef26dba3d3d
Justine <jawunudo@gmail.com>
2022-10-20 06:27:44 -0700

    0 t0 9 no printf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers from 0 - 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
