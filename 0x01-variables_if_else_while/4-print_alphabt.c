ab233bfa9e84b3bbdd5350f777f3a570763925d8
Justine <jawunudo@gmail.com>
2022-10-20 04:08:26 -0700

    alphabets without e and ?

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets A to Z in lower case without e and q
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		while (a !='e' && a !== 'q')
+		{
+		putchar(letter);
+		}
+
+	putchar('\n');
+	return (0);
+}
