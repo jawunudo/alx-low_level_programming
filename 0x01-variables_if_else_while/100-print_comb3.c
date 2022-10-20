06180cd65d1080c2da05d98fec3526e93c451dff
Justine <jawunudo@gmail.com>
2022-10-20 10:40:42 -0700

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,8 @@ int main(void)
 {
 	int n1;
 	int n2;
-
+	for (n1 = 0; n1 < 9; n1++)
+	{
 	for (n2 == n1 + 1; n2 < 10; n2++)
 		{
 			putchar((n1 % 10) + '0');
@@ -19,6 +20,7 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
+	}
 		putchar('\n');
 		return (0);
-	}
+}
