3fe459cdc0ca64a99cd159820958533c85957321
Justine <jawunudo@gmail.com>
2022-10-22 07:13:33 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,14 +5,15 @@
  *
  * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n < 0
  **/
+
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == '0')
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
