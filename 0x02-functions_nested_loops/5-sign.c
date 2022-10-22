b26dfb30c4f4521710644539010c2c2e605e96c0
Justine <jawunudo@gmail.com>
2022-10-22 06:47:04 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n = 0)
+	else if (n = '0')
 	{
 		_putchar('0');
 		return (0);
@@ -20,6 +20,6 @@ int print_sign(int n)
 	else if (n <  '0')
 	{
 		_putchar ("-1");
-		return (-1);
+		return ("-1");
 	}
 }
