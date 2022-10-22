37cba84b8c517b55157e4579e8baca5658f65a97
Justine <jawunudo@gmail.com>
2022-10-22 08:57:09 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * add - adds two numbers
- * @mum1: first number to be added
- * @num2: second number to be added
+ * add - adds two integers
+ * @mum1: first integer to be added
+ * @num2: second integer to be added
  *
  * Return: (num1 + num2)
- **/
+ */
 int add(int num1, int num2)
 {
 	return (num1 + num2);
