8670eca7d6663720d5e2f405541c48d6f8df37a0
Justine <jawunudo@gmail.com>
2022-10-22 04:32:53 -0700

    a-z lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,14 @@
  * Return: always 0
  **/
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char a = 'a';
+	char a ='a'
 
-	while (a <= 'z')
-	{
-		_putchar(a);
-		a++;
-	}
-	_putchar('\n');
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+	_putchar('\n')
 }
