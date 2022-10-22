57667dfd294965d6221ab20dd1d5535042e04df8
Justine <jawunudo@gmail.com>
2022-10-22 04:14:10 -0700

    a-z lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Prints alphabets in lower case
+ *
+ * Return: always 0
+ */
+void print_alphabet(void);
+{
+	char a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+}
