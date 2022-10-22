17135b87150f98a5215e08fb19db35ef271da9c3
Justine <jawunudo@gmail.com>
2022-10-22 05:09:43 -0700

    print alpha X 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter;
 	while (count++ <= 9)
-		for (letter = 'a'; letter <=z: letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-	putchar('\n');
+	_putchar('\n');
 }
