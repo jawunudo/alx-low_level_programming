6cd41a4341752797c5b888e09db3384d6650dcb5
Justine <jawunudo@gmail.com>
2022-10-26 14:34:47 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -20,8 +20,8 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand90 % 78;
-		sum +- (pass[i] + '0');
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
 		putchar(pass[i] = '0');
 		if ((2772 - sum) - '0' < 78)
 		{
