29bf5f21860106c82b51314330cfa5d0c47420fc
Justine <jawunudo@gmail.com>
2022-10-27 13:25:52 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,14 +2,15 @@
 /**
  * print_diagonal - draws a diagonal line in the terminal
  * @n: function argument
- * @a - aditional argument
- * @b - aditional argument
+ * @a: aditional argument
+ * @b: aditional argument
  * Return: no return value.
  */
 
 void print_diagonal(int n)
 {
 	int a, b;
+
 	if (n > 0)
 	{
 		for (a = 0; a < n; a++)
