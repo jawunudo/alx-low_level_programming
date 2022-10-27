60e5f1ae3ec4275c3efe1d41f07da0edfb83cf56
Justine <jawunudo@gmail.com>
2022-10-27 15:22:12 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	while (n = 1; n <= 100; n++)
+	{
+		if (n % 3 = 0)
+		{
+			printf("Fizz");
+		}
+		if (n % 5 = 0)
+		{
+			printf("Buzz");
+		}
+		if (n % 15 = 0)
+		{
+			printf("FizzBuzz")
+		}
+	}
+	printf(' ');
+	printf('\n');
+}
