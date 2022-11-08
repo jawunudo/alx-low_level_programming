ae0e5b4b95c9a4f6c753d88bdb3688c117d89277
Justine <jawunudo@gmail.com>
2022-11-08 11:55:20 -0800

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,31 +2,17 @@
 
 
 /**
-
  * _puts_recursion - function like puts();
-
  * @s: input
-
  * Return: Always 0 (Success)
-
  */
-
 void _puts_recursion(char *s)
-
 {
-
 	if (*s == '\0')
-
 		_putchar('\n');
-
 	else
-
 	{
-
 		_putchar(*s);
-
 		_puts_recursion(s + 1);
-
 	}
-
 }
