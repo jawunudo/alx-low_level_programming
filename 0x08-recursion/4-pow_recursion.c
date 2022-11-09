5ea4fe703f2af87fe4be1a157036371308480651
Justine <jawunudo@gmail.com>
2022-11-09 01:28:59 -0800

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,12 +5,14 @@
  * @x: integer
  * @y: integer
  *
- * return: x**y
+ * Return: x^y
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
+	if (y == 0)
+		return (1);
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
