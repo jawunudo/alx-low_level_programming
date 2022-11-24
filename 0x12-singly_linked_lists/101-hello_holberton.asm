ba9d65bec22d3b3ecb11677133b81fce3a2f8927
Justine <jawunudo@gmail.com>
2022-11-24 07:44:56 -0800

    asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+bal  main
+section .text
+main:
+        mov     rax, 1
+        mov     rdi, 1
+        mov     rsi, message
+        mov     rdx, 17
+	syscall
+
+        mov     rax, 60
+        xor     rdi, rdi
+	syscall
+message:
+        db      "Hello, Holberton", 10
