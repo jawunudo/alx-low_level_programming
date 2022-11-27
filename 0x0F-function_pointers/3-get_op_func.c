3bdf1a4aa74e41e036a123d94801e62d41a00e26
Justine <jawunudo@gmail.com>
2022-11-27 02:00:07 -0800

    3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - return pointer to op_func corresponding to `s'
+ * @s: string representing desired operation
+ *
+ * Return: pointer to op_func taking two ints and returning an int,
+ * or NULL if string does not correspond to a valid operation.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
+			return (ops[i].f);
+		++i;
+	}
+	return (NULL);
+}
