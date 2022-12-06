503984a28b50e03d1e03d21d8955a08bb0160d6e
Justine <jawunudo@gmail.com>
2022-12-06 03:29:14 -0800

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+nclude <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * create_file - Create `filename' with content from `text_content'
+ * @filename: name of file to create
+ * @text_content: content to insert into file `filename'
+ *
+ * Return: 1 on success, -1 if error occurs using `write`.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	size_t i = 0;
+	ssize_t w = 0;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	for (i = 0; text_content && text_content[i]; i++)
+		;
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (i)
+		w = write(fd, text_content, i);
+	close(fd);
+	if (w == -1)
+		return (-1);
+	return (1);
+}
